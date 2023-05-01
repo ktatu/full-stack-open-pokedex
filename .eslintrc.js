8e3c00f608879555b46a9d73f01392a2b24a7c2d
ktatu <kaikkonentatu@gmail.com>
2023-05-01 17:02:40 +0300

    eslint fixes and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         "jest/globals": true,
         
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
+    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:cypress/recommended"],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
