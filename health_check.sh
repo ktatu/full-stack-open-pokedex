a244946146f8f9c41d74c5d7b79c423918ababff
ktatu <kaikkonentatu@gmail.com>
2023-05-06 03:28:31 +0300

    Modifying health check script file

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+response=$(curl --silent http://localhost:5000)
 
-exit 0 # exit status 1 means that the script "fails"
+if [[ "$response" == *"ok"* ]]; then
+  echo "Health check passed"
+  exit 0
+else
+  echo "Health check failed"
+  exit 1
+fi
