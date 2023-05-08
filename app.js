527da61d60f3d30e46ab199ff75e240d4dfac0c1
ktatu <kaikkonentatu@gmail.com>
2023-05-08 04:38:20 +0300

    Add eslint rule violating test code

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/version', (req, res) => {
   res.send('1')
 })
 
+app.get("/test", (req, res) => {
+    res.send("ok")
+})
+
 app.listen(PORT, () => {
   //eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
